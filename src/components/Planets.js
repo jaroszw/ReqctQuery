e583560cdda9ae161c92abdd8ad82d417f18b521
Wojciech Jarosz <jaroszw@gmail.com>
2022-04-03 19:09:05 +0200

    reactQuery variables

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Planet from './Planet';
 import { useQuery } from 'react-query';
 
-const fetchPlanets = async () => {
-  const res = await fetch('https://swapi.dev/api/planets');
-  return res.json();
+const fetchPlanets = async ({ queryKey }) => {
+  const [key, page] = queryKey;
+
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
+  const data = await res.json();
+
+  return data;
 };
 
 const Planets = () => {
-  const obj = useQuery('planets', fetchPlanets, {
-    staleTime: 2000,
-    onSuccess: () => console.log('data fetched with no problems'),
-  });
-
-  console.log(obj);
+  const [page, setPage] = useState(1);
+  const obj = useQuery(['planets', page], fetchPlanets);
 
   return (
     <div>
       <h2>Planets</h2>
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
       {obj.status === 'loading' && <div>Data fetching in progress</div>}
       {obj.status === 'error' && <div>Error fetching data</div>}
       {obj.status === 'success' && (
